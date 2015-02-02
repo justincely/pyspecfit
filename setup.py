f9beac4ac3090ae00f3edc0ec397b484c85f316e
Justin Ely <ely@stsci.edu>
2015-02-02 10:31:12 -0500

    package scripts added

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import os
+import glob
+
+setup(
+    name = 'pyspecfit',
+    url = 'http://justincely.github.io',
+    version = '0.0.1',
+    description = 'interact with IRAF task specfit I/O products',
+    author = 'Justin Ely',
+    author_email = 'ely@stsci.edu',
+    keywords = ['astronomy'],
+    classifiers = ['Programming Language :: Python',
+                   'Development Status :: 1 - Planning',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: Astronomy',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Topic :: Software Development :: Libraries :: Python Modules'],
+    packages = ['pyspecfit'],
+    requires = ['numpy','scipy','astropy']
+    )
